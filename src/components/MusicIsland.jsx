3668776f1530adc804dc2b19e35e28c8f47520f5
veggexplay <olivedelarosa500@gmail.com>
2025-06-24 23:08:23 -0600

    fix: fixed MusicIsland behavior

--- src/components/MusicIsland.jsx
+++ src/components/MusicIsland.jsx
@@ -3,10 +3,10 @@ import { Heart, Pause, Play } from "../icons/Library";
 import { useFetch } from "../hooks/useFetch";
 import { useState, useMemo } from "react";
 import { ControlBar } from "./ControlBar";
-import {hexToRgba} from "../utils/colors"
+import { hexToRgba } from "../utils/colors";
 
 export function MusicIsland() {
-  const [isExpanded, setIsExpanded] = useState(false)
+  const [isExpanded, setIsExpanded] = useState(false);
   const { artists } = useFetch();
 
   const {
@@ -21,29 +21,30 @@ export function MusicIsland() {
 
   const playPauseIcon = isPaused ? <Play /> : <Pause />;
 
-  const classExpanded = isExpanded ? "musicIslandExpand" : ""
+  const classExpanded = isExpanded ? "musicIslandExpand" : "";
 
   const progress = useMemo(() => {
-    return (currentTime / duration) * 100
-  }, [currentTime, duration])
+    return (currentTime / duration) * 100;
+  }, [currentTime, duration]);
 
-  const {currentArtist, artistColor, rgbaColor} = useMemo(() => {
-    if (!currentSong || !artists) return { currentArtist: null, artistColor: null, rgbaColor:null }
+  const { currentArtist, artistColor, rgbaColor } = useMemo(() => {
+    if (!currentSong || !artists)
+      return { currentArtist: null, artistColor: null, rgbaColor: null };
 
     const currentArtist = Array.isArray(currentSong.artist)
-    ? currentSong.artist[0]
-    : currentSong.artist;
+      ? currentSong.artist[0]
+      : currentSong.artist;
 
     const artistColor = artists.find(
       (artist) => artist.artist === currentArtist
     )?.color;
 
-    const rgbaColor = hexToRgba(artistColor, 0.3)
+    const rgbaColor = hexToRgba(artistColor, 0.3);
 
-    return {currentArtist, artistColor, rgbaColor}
-  }, [currentSong, artists])
+    return { currentArtist, artistColor, rgbaColor };
+  }, [currentSong, artists]);
 
-  if (!currentSong || !artists) return null
+  if (!currentSong || !artists) return null;
 
   const handlePlay = () => {
     setIsPaused(!isPaused);
@@ -61,72 +62,88 @@ export function MusicIsland() {
   };
 
   const handleMouseDown = (e) => {
-    e.preventDefault()
-  }
+    e.preventDefault();
+  };
 
   const handleClassExpanded = () => {
-    setIsExpanded(!isExpanded)
-  }
+    setIsExpanded(!isExpanded);
+  };
 
   return (
-    <div
-      tabIndex={0}
-      onClick={handleClassExpanded}
-      onBlur={() => setIsExpanded(false)}
-      style={{ background: rgbaColor }}
-      className={`absolute transition-all duration-150 bottom-4 left-1/2 -translate-x-1/2 w-full h-15 max-w-[95%] rounded-md sm:hidden flex flex-col gap-4 z-20 overflow-hidden ${classExpanded} backdrop-blur-lg`}
-    >
+    <div className="absolute pointer-events-none h-full w-full">
       <div
-        className="flex relative flex-col h-15 w-full"
+        tabIndex={0}
+        onClick={handleClassExpanded}
+        onBlur={() => setIsExpanded(false)}
+        className={`absolute pointer-events-none transition-all duration-150 bottom-0 py-3 px-3 w-full h-20 sm:hidden flex flex-col gap-4 z-20 ${classExpanded}`}
       >
-        <div className="flex w-full h-full justify-between p-2">
-          <section className="flex items-center gap-3">
-            <picture className="flex h-full aspect-square rounded-md overflow-hidden">
-              <img
-                className="h-full w-full object-cover"
-                src={currentSong.cover}
-                alt=""
-              />
-            </picture>
-            <div>
-              <strong>{currentSong.title}</strong>
-              <p>{currentSong.artist}</p>
+        <div
+          style={{ background: rgbaColor }}
+          className="flex pointer-events-auto relative flex-col h-full rounded-md w-full backdrop-blur-lg overflow-hidden"
+        >
+          <div className="flex relative flex-col h-15 rounded-md w-full">
+            <div className="flex w-full h-full justify-between p-2">
+              <section className="flex items-center gap-3">
+                <picture className="flex h-full aspect-square rounded-md overflow-hidden">
+                  <img
+                    className="h-full w-full object-cover"
+                    src={currentSong.cover}
+                    alt=""
+                  />
+                </picture>
+                <div>
+                  <strong>{currentSong.title}</strong>
+                  <p>{currentSong.artist}</p>
+                </div>
+              </section>
+              <section className="flex items-center gap-3">
+                <button
+                  aria-label="Marcar como favorito"
+                  onMouseDown={handleMouseDown}
+                  className="h-6"
+                >
+                  <Heart />
+                </button>
+                <button
+                  aria-label={isPaused ? "Reproducir" : "Pausar"}
+                  onMouseDown={handleMouseDown}
+                  className="h-6"
+                  onClick={handleButtons}
+                >
+                  {playPauseIcon}
+                </button>
+              </section>
             </div>
-          </section>
-          <section className="flex items-center gap-3">
-            <button aria-label="Marcar como favorito" onMouseDown={handleMouseDown} className="h-6">
-              <Heart />
-            </button>
-            <button aria-label={isPaused ? "Reproducir" : "Pausar"} onMouseDown={handleMouseDown} className="h-6" onClick={handleButtons}>
-              {playPauseIcon}
-            </button>
-          </section>
-        </div>
-        <div className="absolute bottom-0 left-0 w-full h-1 bg-gray-500/75 rounded overflow-hidden">
-          <div
-            className="h-full bg-white transition-all duration-300"
-            style={{ width: `${progress}%` }}
-          ></div>
-        </div>
-      </div>
-      <div className="h-full p-4 overflow-hidden">
-        <div className="flex h-full flex-col gap-8">
-          <div className="flex h-1/2 flex-col items-center gap-4">
-            <picture className="flex h-full bg-blue-500 aspect-square rounded-md overflow-hidden">
-              <img
-                className="h-full w-full object-cover"
-                src={currentSong.cover}
-                alt=""
-              />
-            </picture>
-            <div className="text-center">
-              <strong className="text-3xl">{currentSong.title}</strong>
-              <p className="font-semibold">{currentSong.artist}</p>
+            <div className="absolute bottom-0 left-0 w-full h-1 bg-gray-500/75 rounded overflow-hidden">
+              <div
+                className="h-full bg-white transition-all duration-300"
+                style={{ width: `${progress}%` }}
+              ></div>
             </div>
           </div>
-          <div onMouseDown={handleMouseDown} className="flex flex-col h-1/2 w-full justify-center items-center">
-            <div className="w-full">
-              <ControlBar />
+          <div className="flex flex-col justify-center h-full p-4 overflow-hidden">
+            <div className="flex flex-col gap-8">
+              <div className="flex h-2/3 flex-col justify-end items-center gap-4">
+                <picture className="flex h-full bg-blue-500 aspect-square rounded-md overflow-hidden">
+                  <img
+                    className="h-full w-full object-cover"
+                    src={currentSong.cover}
+                    alt=""
+                  />
+                </picture>
+                <div className="text-center">
+                  <strong className="text-3xl">{currentSong.title}</strong>
+                  <p className="font-semibold">{currentSong.artist}</p>
+                </div>
+              </div>
+              <div
+                onMouseDown={handleMouseDown}
+                className="flex flex-col h-full w-full items-center"
+              >
+                <div className="w-full">
+                  <ControlBar />
+                </div>
+              </div>
             </div>
           </div>
         </div>
